30b23b43b2fd17a6cd6fd73f0fda3e464d8f3acf
tonny wachira <kimamo@live.com>
2016-01-10 11:06:48 -0500

    expanded date range

--- plot2.R
+++ plot2.R
@@ -1,34 +1,35 @@
 
 plot2 <- function(){
-  dataFilePath <- getwd()
-  consumptionData <-
+  rawFilePath <- getwd()
+  rawData <-
     read.table(
-      "./data/household_power_consumption.txt", header = TRUE, sep = ';',
+      "../data/household_power_consumption.txt", header = TRUE, sep = ';',
       stringsAsFactors = FALSE, dec = ".", comment.char = "", quote = '\"', check.names =
         FALSE, na.strings = "?"
     )
   
-  consumptionData$Date <- as.Date(consumptionData$Date, format = "%d/%m/%Y")
+  rawData$Date <- as.Date(rawData$Date, format = "%d/%m/%Y")
 
-  #Get a subset of the data
+#Get a subset of the data
   dataSubset <-
-    subset(consumptionData, subset = (Date >= "2006-12-01" &
-                                        Date <= "2006-12-16"))
+    subset(rawData, subset = (Date >= "2006-12-01" &
+                                        Date <= "2006-12-31"))
   dates <- paste(as.Date(dataSubset$Date),dataSubset$Time)
   
   dataSubset$Datetime <- as.POSIXct(dates) 
   
-  #remove the fully loaded data object from memory
-  rm(consumptionData)
+#remove the fully loaded data object from memory
+  rm(rawData)
   
   activePower <- as.numeric(dataSubset$Global_active_power)
-   
-  quartz("Quartz", width = 5, height = 4, pointsize = 18)
-  
+#turnOn the Mac Screen   
+  quartz("Quartz", width = 8, height = 7, pointsize = 21)
+
+#plot graph
   plot(activePower~dataSubset$Datetime, type="l", ylab = "Global Active Power (Kw)", xlab="") 
   
-  #Save file
-  dev.copy(png, file = "./ExData_Plotting1/plot2.png", height = 480, width = 480)
+#Save file
+  dev.copy(png, file = "plot2.png", height = 480, width = 480)
   dev.off()
   
 } 
